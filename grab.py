7c9aa0e92386cd641cdcfcb153236a670f0f8669
Iyamoto <Iyamoto@users.noreply.github.com>
2014-04-02 16:25:59 +0400

    2

--- grab.py
+++ grab.py
@@ -9,7 +9,7 @@ def url2file(url, html, png=''):
     html - where to save page code (html)
     png - where to save rendered page (image)"""
     #code = subprocess.call(['save.cmd', url, html, png])
-    cmd = './save.sh'+url+' '+html
+    cmd = './save.sh '+url+' '+html
     #code = subprocess.call(['./save.sh', url, html, png], shell=True)
     code = subprocess.call(cmd, shell=True)
     # TODO  check return code
