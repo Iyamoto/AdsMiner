9475e0da05363119fc11fa0b857082d9831b6426
Iyamoto <Iyamoto@users.noreply.github.com>
2014-04-03 09:26:33 +0400

    add dev-null

--- save.sh
+++ save.sh
@@ -3,6 +3,6 @@ phantom='/usr/local/phantomjs/bin/phantomjs'
 param='--cookies-file=cookies.txt --disk-cache=true --proxy-type=none'
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
 echo 1 > /proc/sys/net/ipv6/conf/default/disable_ipv6
-$phantom $param save.js $1 $2
+$phantom $param save.js $1 $2 > /dev/null 2>&1
 echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6
 echo 0 > /proc/sys/net/ipv6/conf/default/disable_ipv6
