afbec95ca428713beb2bdaf9c02c3513345b3cda
Iyamoto <Iyamoto@users.noreply.github.com>
2014-04-04 10:32:39 +0400

    separate tests

--- test-grab.py
+++ test-grab.py
@@ -6,6 +6,25 @@ import hashlib
 import os.path
 import adsminer
 
+# Test Harness
+test_data = {}
+test_data['http://gotovim-doma.ru/']=9
+test_data['http://horo.tochka.net/']=2 #????
+test_data['http://www.edimdoma.ru/retsepty']=4
+test_data['http://www.mycharm.ru/']=4
+test_data['http://onbeauty.ru/']=6
+test_data['http://yeswoman.ru/']=11
+test_data['http://www.astrocentr.ru/']=8
+test_data['http://astroscope.ru/']=13
+test_data['http://www.sonniki.net.ru/']=4
+
+def test_parseBlocks(url,num):
+    test_data[url] = test_data.get(url, 0)
+    msg = 'Testing! '+url+' Should be: ' + str(test_data[url]) + ' Got: ' + str(num)
+    print(msg)
+    diff = abs(num - test_data[url])
+    return diff
+
 # Read config
 config = configparser.ConfigParser()
 try:
@@ -24,27 +43,24 @@ maxBlockSize = int(config['GRABBER']['MaxBlockSize'])
 minBlockSize = int(config['GRABBER']['MinBlockSize'])
 maxLinks = int(config['GRABBER']['MaxLinks'])
 
-isLogFile = adsminer.initLog(log_file)
-
 # TODO add multi lists support
 
-urls = adsminer.file2list(urlsfile)
+urls = test_data.keys()
 if len(urls)==0:
     print('No urls found')
     assert False
 
-total_blocks = 0
+total_diff = 0
 
 for url in urls:
     try:
         url = adsminer.clearUrl(url)
     except:
         continue
-    adsminer.writeLog(log_file, url+'\n', isLogFile)
     url_id = hashlib.md5(url.encode('utf-8')).hexdigest()    
     path = datadir + url_id + '.html'
 
-    print(url, path)
+    #print(url, path)
     if os.path.isfile(path) == False:
         code = adsminer.url2file(run, url, path)
         if code==False:
@@ -59,18 +75,12 @@ for url in urls:
     # TODO add tidy html?
     ads = adsminer.parseBlocks(text, url, block_complexity, minBlockSize, maxBlockSize, maxLinks)
     ads_num = len(ads.keys())
-    total_blocks +=ads_num
-      
-    adsminer.writeLog(log_file, 'Find ads: '+str(ads_num)+'\n', isLogFile)
     
-    if ads_num>0:
-        for id in ads.keys():
-            if isLogFile:
-                out = adsminer.getBlock(id,ads)
-                adsminer.writeLog(log_file, out, isLogFile)
+    # Test Harness
+    diff = test_parseBlocks(url,ads_num)
+    total_diff += diff
 
     del(ads)
     #break
 
-adsminer.writeLog(log_file, 'Total blocks found: '+str(total_blocks)+'\n', isLogFile)
-adsminer.writeLog(log_file, 'Done\n', isLogFile)
+print('Total diff: '+str(total_diff))
