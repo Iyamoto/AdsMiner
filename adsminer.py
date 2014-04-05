5f8017d76a3a4b2b08b26f60fffaa768cbfab7ac
Iyamoto <Iyamoto@users.noreply.github.com>
2014-04-05 19:04:58 +0300

    latter

--- adsminer.py
+++ adsminer.py
@@ -7,8 +7,8 @@ import codecs
 import re
 import os.path
 import json
-from tidylib import tidy_document
-import chardet
+#from tidylib import tidy_document
+#import chardet
 
 def uniqList(lst):
     assert type(lst)==list
@@ -59,7 +59,7 @@ def url2html(run, url, path):
         assert code==True
     try:
         html = file2text(path)
-        html, errors = tidy_document(html, options={'hide-comments':1, 'new-inline-tags':'yatag'})
+        #html, errors = tidy_document(html, options={'hide-comments':0, 'new-inline-tags':'yatag'})
         #print(errors)
     except:
         print('Cant read file: '+path)
@@ -77,21 +77,21 @@ def file2text(path):
     f.close()
     return text
 
-def file2textU(path):
-    """Reads file from path
-    Detects encoding
-    Returns utf-8 text"""
-    try:
-        f = open(path, 'rb')
-    except:
-        print('Cant read file: '+path)
-        return ''
-    rawdata = f.read()
-    result = chardet.detect(rawdata)
-    charenc = result['encoding']
-    f.close()
-    text = rawdata.decode(charenc)
-    return text
+##def file2textU(path):
+##    """Reads file from path
+##    Detects encoding
+##    Returns utf-8 text"""
+##    try:
+##        f = open(path, 'rb')
+##    except:
+##        print('Cant read file: '+path)
+##        return ''
+##    rawdata = f.read()
+##    result = chardet.detect(rawdata)
+##    charenc = result['encoding']
+##    f.close()
+##    text = rawdata.decode(charenc)
+##    return text
 
 def file2list(path):
     """Reads utf-8 file from path
