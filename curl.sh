8148ef84b9cd8deecf72e81a8c47e03bd2f9f6d3
root <root@localhost.localdomain>
2014-04-06 08:21:57 -0400

    no redirects found

--- curl.sh
+++ curl.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# cat list_test |xargs -n1 -i bash -k curl.sh  {}
+# cat  tasks/targets.wget |xargs -n1 -i bash -k curl.sh  {}
 
 echo $1
-curl -s -i -A 'Mozilla/4.0' -L $1  |grep "200 OK" -B 100 | tac |grep -m 1 -i "Location: http"
+curl -s -i -A 'Mozilla/5.0 (Windows NT 5.1; rv:27.0) Gecko/20100101 Firefox/27.0' -L $1  |grep "200 OK" -B 100 | tac |grep -m 1 -i "Location: http"; 
+if [ $? -ne "0" ]
+then
+ echo "No redirect found"
+fi
