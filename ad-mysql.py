5a2dfdd87611f7254076f4d2665fb6954ca4d917
Iyamoto <Iyamoto@users.noreply.github.com>
2014-04-07 15:28:18 +0400

    pass

--- ad-mysql.py
+++ ad-mysql.py
@@ -11,20 +11,20 @@ except:
 
 host = config['MYSQL']['host']
 user = config['MYSQL']['user']
-pass = config['MYSQL']['pass']
+password = config['MYSQL']['pass']
 db = config['MYSQL']['db']
 
 try:
-    db = MySQLdb.connect(host, user, pass, db, charset='utf8')
+    db = MySQLdb.connect(host, user, password, db, charset='utf8')
 except:
     print('Cant connect to db')
     assert False
 
 ##cursor = db.cursor()
 ##
-##sql = """INSERT INTO contacts(name, mail, adres, tel)
-##        VALUES ('%(name)s', '%(mail)s', '%(adres)s', '%(tel)s')
-##        """%{"name":fname, "mail":fmail, "adres":fadres, "tel":ftel}
+##sql = """INSERT INTO sites(domain)
+##        VALUES ('%(name)s')
+##        """%{"name":domain}
 ##cursor.execute(sql)
 
 
