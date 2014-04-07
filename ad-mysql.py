82ab3c8a9524bd586e2eeb88d189fb08b1b3f2e5
Iyamoto <Iyamoto@users.noreply.github.com>
2014-04-07 15:46:02 +0400

    test

--- ad-mysql.py
+++ ad-mysql.py
@@ -1,35 +1,51 @@
 import pymysql
 import configparser
 
-# Read config
-config = configparser.ConfigParser()
-try:
-    config.read('miner.conf')
-except:
-    print('Cant read config file')
-    assert False
+def init_mysqldb():
+    """ Connects to mysql db """
+    # Read config
+    config = configparser.ConfigParser()
+    try:
+        config.read('miner.conf')
+    except:
+        print('Cant read config file')
+        assert False
 
-host = config['MYSQL']['host']
-user = config['MYSQL']['user']
-password = config['MYSQL']['pass']
-db = config['MYSQL']['db']
+    host = config['MYSQL']['host']
+    user = config['MYSQL']['user']
+    password = config['MYSQL']['pass']
+    db = config['MYSQL']['db']
 
-try:
-    conn = pymysql.connect(host, user, password, db)
-except:
-    print('Cant connect to db')
-    assert False
+    try:
+        conn = pymysql.connect(host, user, password, db)
+    except:
+        print('Cant connect to db')
+        assert False
+    return conn
 
-cur = conn.cursor()
+def write2mysql(conn, sql):
+    """ executes sql request"""
+    cur = conn.cursor()
+    cur.execute(sql)
+    for response in cur:
+        print(response)
+    cur.close()
+    return
 
-domain = 'www.ru'
+def kill_mysqldb(conn):
+    """ Finishes connection to db"""
+    conn.close()
+
+conn = init_mysqldb()    
+
+domain = 'ya.ru'
 sql = """INSERT INTO sites(domain)
         VALUES ('%(name)s')
         """%{"name":domain}
 
-cur.execute(sql)
-for response in cur:
-    print(response)
+write2mysql(conn, sql)
+kill_mysqldb(conn)
 
-cur.close()
-conn.close()    
+
+
+   
