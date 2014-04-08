95fa79c3dd0e5380006cf12a5b204330b9b16bb5
Iyamoto <Iyamoto@users.noreply.github.com>
2014-04-08 09:31:23 +0400

    insert

--- json2alchemy.py
+++ json2alchemy.py
@@ -27,8 +27,9 @@ metadata = MetaData(db)
 
 # Get all sites
 sites_table = Table('sites', metadata, autoload=True)
-s = sites_table.select()
-rs = s.execute()
+sel = sites_table.select()
+ins = sites_table.insert()
+rs = sel.execute()
 rows = rs.fetchall()
 
 sites = {}
@@ -36,47 +37,40 @@ for row in rows:
     sites[row[1]] = row[0] # sites[domain]=id
 print(sites)
 
-##conn = adsmysql.init_mysqldb()
-##write2db = adsmysql.write2mysql
-##
-##sql = 'SELECT * FROM sites'
-##data = adsmysql.execute_mysqldb(conn, sql)
-##print(data)
-##    
-##json_dir = 'json'
-##
-##total = 0
-##for file in os.listdir(json_dir):
-##    if file.endswith('.txt'):
-##        print('Reading json file: ',file)
-##        json_path = os.path.join(json_dir, file)
-##        if os.path.isfile(json_path) == True:
-##            data = adsminer.readJson(json_path)
-##            for block in data:
-####                try:
-##                ab = adsminer.adblock(block[0],block[1])
-##                for img in block[3]:
-##                    ab.addImgUrl(img)
-##                i=0
-##                for link in block[2]:
-##                    ab.addLink(link, block[5][i])
-##                    i+=1
-##                ab.addText(block[4])
-##                #print(ab.getSrcDomain())
-##                row = []
-##                row.append(('domain',ab.getSrcDomain()))
-##                write2db(conn, 'sites', row)
-##                break
-##                total+=1
-####                except:
-####                    print('Cant get block')
-####                    continue
-##
-##print('Total Links found: ',  total)
-##
-##
-##adsmysql.kill_mysqldb(conn)
-##
-##            
+    
+json_dir = 'json'
+
+total = 0
+for file in os.listdir(json_dir):
+    if file.endswith('.txt'):
+        print('Reading json file: ',file)
+        json_path = os.path.join(json_dir, file)
+        if os.path.isfile(json_path) == True:
+            data = adsminer.readJson(json_path)
+            for block in data:
+##                try:
+                ab = adsminer.adblock(block[0],block[1])
+                for img in block[3]:
+                    ab.addImgUrl(img)
+                i=0
+                for link in block[2]:
+                    ab.addLink(link, block[5][i])
+                    i+=1
+                ab.addText(block[4])
+                #Check if domain is already in the db
+                if ab.getSrcDomain() not in sites.keys():
+                    #Insert into the db
+                    r = ins.execute(domain=ab.getSrcDomain())
+                    print(r)
+                
+                break
+                total+=1
+##                except:
+##                    print('Cant get block')
+##                    continue
+
+print('Total Links found: ',  total)
+
+          
 
 
