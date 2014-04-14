114a42e1e77a844e43ff4d9b73d4659fd99016fd
Iyamoto <Iyamoto@users.noreply.github.com>
2014-04-14 10:05:19 +0400

    wrapers tuning

--- grab.sh
+++ grab.sh
@@ -3,8 +3,8 @@
 phantom='/usr/local/phantomjs/bin/phantomjs'
 param='--cookies-file=cookies.txt --disk-cache=false --proxy-type=none'
 outdir='/tmp/phantom/'
-timeout='5000'
-threads='2'
+timeout='3000'
+threads='1'
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
 echo 1 > /proc/sys/net/ipv6/conf/default/disable_ipv6
 $phantom $param grab.js $1 $outdir $timeout $threads > /dev/null 2>&1
