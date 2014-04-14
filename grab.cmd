f013280323afe0c6ca82bac1d2ba31eaac649279
Iyamoto <Iyamoto@users.noreply.github.com>
2014-04-14 11:12:28 +0400

    nice

--- grab.cmd
+++ grab.cmd
@@ -2,6 +2,6 @@ rem grab html from urls list with phantom.js
 set params=--cookies-file=cookies.txt --disk-cache=false --proxy-type=none
 set outdir=F:\tmp\py\
 set timeout=3000
-set threads=1
+set threads=3
 
 D:\tools\phantomjs-1.9.7-windows\phantomjs.exe %params% grab.js %1 %outdir% %timeout% %threads%
--- grab.sh
+++ grab.sh
@@ -4,9 +4,9 @@ phantom='/usr/local/phantomjs/bin/phantomjs'
 param='--cookies-file=cookies.txt --disk-cache=false --proxy-type=none'
 outdir='/tmp/phantom/'
 timeout='3000'
-threads='1'
+threads='3'
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
 echo 1 > /proc/sys/net/ipv6/conf/default/disable_ipv6
-$phantom $param grab.js $1 $outdir $timeout $threads > /dev/null 2>&1
+nice $phantom $param grab.js $1 $outdir $timeout $threads > /dev/null 2>&1
 echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6
 echo 0 > /proc/sys/net/ipv6/conf/default/disable_ipv6
